fffe3fd6d469a1c21c87d7a9086d03297bf3fed2
Drew Poland <drew.poland@gmail.com>
2012-11-19 03:54:15 -0500

    admin page settings addition

--- wp-mailcheck-admin.php
+++ wp-mailcheck-admin.php
@@ -4,8 +4,8 @@ add_action('admin_menu', 'mailcheck_create_menu');
 
 function mailcheck_create_menu() {
 
-	//create new top-level menu
-	add_menu_page('Mailcheck Plugin Settings', 'Mailcheck', 'administrator', __FILE__, 'mailcheck_settings_page',plugins_url('/images/icon.png', __FILE__));
+	//create new top-level menu -- SHOULD MAKE THIS SUBMENU INSTEAD OF TOP LEVEL ?
+	add_menu_page('Mailcheck Plugin Settings', 'Mailcheck', 'administrator', __FILE__, 'mailcheck_settings_page',plugins_url('settings.png', __FILE__));
 
 	//call register settings function
 	add_action( 'admin_init', 'register_mailcheck_settings' );
@@ -22,32 +22,32 @@ function register_mailcheck_settings() {
 function mailcheck_settings_page() {
 ?>
 <div class="wrap">
-<h2>Mailcheck WordPress Plugin Options</h2>
-
-<!-- see action on line 27 for filename of options -->
-
-<form method="post" action="wp-mailcheck-options.php">
-    <?php settings_fields( 'mailcheck-settings-group' ); ?>
-    <?php do_settings( 'mailcheck-settings-group' ); ?>
-    <table class="form-table">
-        <tr valign="top">
-        <th scope="row">New Option Name</th>
-        <td><input type="text" name="new_option_name" value="<?php echo get_option('new_option_name'); ?>" /></td>
-        </tr>
+	<h2>Mailcheck WordPress Plugin Options</h2>
+		<div class="left">
+			<form method="post" action="wp-mailcheck-options.php">
+			    <?php settings_fields( 'mailcheck-settings-group' ); ?>
+			    <?php do_settings( 'mailcheck-settings-group' ); ?>
+			    <table class="form-table">
+			        <tr valign="top">
+			        <th scope="row">Domains:</th>
+			        <td><input type="text" name="option1" value="<?php echo get_option('mailcheck_domain_inputs'); ?>" /></td>
+			        </tr>
          
-        <tr valign="top">
-        <th scope="row">Some Other Option</th>
-        <td><input type="text" name="some_other_option" value="<?php echo get_option('some_other_option'); ?>" /></td>
-        </tr>
+			        <tr valign="top">
+			        <th scope="row">Top Level Domains:</th>
+			        <td><input type="text" name="option2" value="<?php echo get_option('mailcheck_tld_inputs'); ?>" /></td>
+			        </tr>
         
-        <tr valign="top">
-        <th scope="row">Options, Etc.</th>
-        <td><input type="text" name="option_etc" value="<?php echo get_option('option_etc'); ?>" /></td>
-        </tr>
-    </table>
-    
-    <?php submit_button(); ?>
-
-</form>
-</div>
+			        <tr valign="top">
+			        <th scope="row">Activate for WordPress Registration Screen?</th>
+			        <td><input type="checkbox" name="option3" value="<?php echo get_option('mailcheck_activate_on_wplogin'); ?>" /></td>
+			        </tr>
+			    </table>
+			    <?php submit_button(); ?>
+			</form>
+		</div> <!-- left -->
+		<div class="right">
+			<p> Box here with links to Github, Sizeable, and Maintainer ? </p>
+		</div> <!-- right -->
+</div> <!-- wrap -->
 <?php } ?>
