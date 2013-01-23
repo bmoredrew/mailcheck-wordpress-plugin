a8b56614c2e8d35d0e50f920c502136f712064c0
Drew Poland <drew.poland@gmail.com>
2013-01-23 17:14:38 -0500

    credit adjustment

--- wp-mailcheck.php
+++ wp-mailcheck.php
@@ -9,7 +9,7 @@ Author URI: http://www.baltimoredrew.com
 License: GPL2
 */
 
-/*  Copyright 2013  Drew Poland  & Andy Stratton (http://theandystratton.com)
+/*  Copyright 2013 Sizeable Interactive (http://sizeableinteractive.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
