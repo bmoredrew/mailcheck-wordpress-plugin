292605a1349ef696929fc66d340876266e17ccfd
Joe Lee <grandmstramrod@gmail.com>
2016-05-06 15:40:19 +0100

    .live() depreciated changed to .on()

--- views/mailcheck-inline-script.php
+++ views/mailcheck-inline-script.php
@@ -1,46 +1,46 @@
 <script type="text/javascript">
 jQuery(document).ready(function($){
-	
+
 	var domains = ["yahoo.com", "google.com", "hotmail.com", "gmail.com", "me.com", "aol.com", "mac.com",
       "live.com", "comcast.net", "googlemail.com", "msn.com", "hotmail.co.uk", "yahoo.co.uk",
       "facebook.com", "verizon.net", "sbcglobal.net", "att.net", "gmx.com", "mail.com", "outlook.com"];
 
 	var topLevelDomains = ["co.uk", "com", "net", "org", "info", "edu", "gov", "mil"];
-	
+
 	// var superStringDistance = function(string1, string2) {
 	// string distance algorithm of your choosing
 	// }
 	var selector = '<?php echo esc_js( $this->selector ); ?>';
 	$(selector).on('blur', function(){
-		
+
 		$(this).mailcheck({
 			domains: domains,                       
-			topLevelDomains: topLevelDomains,       
+			topLevelDomains: topLevelDomains,
 			//distanceFunction: superStringDistance,
 	    	suggested: function(element, suggestion) {
-		
+
 				var $parent = $(selector).parent();
 				$('.szbl-mailcheck-msg', $parent).remove();
 				$parent.append('<span class="szbl-mailcheck-msg">Did you mean <a href="#" class="szbl-mailcheck-suggestion">' + suggestion.full + '</a>?</span>');
-				
+
 		    },
 		    empty: function(element) {
-			
+
 				var $parent = $(selector).parent();
 				$('.szbl-mailcheck-msg', $parent).remove();
 				if ( !$(selector).val().match(/^[\w\d\.\-\_']+@([\w\d\-]+\.)+\w{2,}$/) )
 				$parent.append('<span class="szbl-mailcheck-msg szbl-mailcheck-error">Please enter a valid email address.</span>');
-				
+
 		    }
 		});
-		
-	});	
-	
-	$('a.szbl-mailcheck-suggestion').live('click', function(){
+
+	});
+
+	$('a.szbl-mailcheck-suggestion').on('click', function(){
 		$(selector).val( $(this).html() );
 		$(this).parent().remove();
 		return false;
 	});
-	
+
 });
 </script>
--- wp-mailcheck-main.php
+++ wp-mailcheck-main.php
@@ -2,14 +2,14 @@
 class Szbl_Mailcheck_Js
 {
 	public static $instance;
-	
+
 	public static function getInstance()
 	{
-		if ( is_null( self::$instance ) ) 
+		if ( is_null( self::$instance ) )
 			self::$instance = new Szbl_Mailcheck_Js();
 		return self::$instance;
 	}
-	
+
 	public function __construct()
 	{
 		add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ) );
@@ -17,30 +17,30 @@ class Szbl_Mailcheck_Js
 		add_action( 'login_enqueue_scripts', array( $this, 'login_enqueue_scripts' ) );
 		add_action( 'login_head', array( $this, 'login_head' ) );
 	}
-	
+
 	public function wp_enqueue_scripts()
 	{
 		wp_enqueue_script( 'jquery' );
 		wp_register_script( 'szbl-mailcheck-js', plugins_url( '/js/mailcheck.min.js', __FILE__ ), array( 'jquery' ), true );
 		wp_enqueue_script( 'szbl-mailcheck-js' );
 	}
-	
+
 	public function wp_footer()
 	{
 		$this->selector = '#email';
 		include dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'mailcheck-inline-script.php';
 	}
-	
+
 	public function login_enqueue_scripts()
 	{
 		$this->wp_enqueue_scripts();
 	}
-	
+
 	public function login_head()
 	{
 		$this->selector = '#user_login';
 		include dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'mailcheck-inline-script.php';
 	}
-	
+
 }
 Szbl_Mailcheck_Js::getInstance();
